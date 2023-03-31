cbff3c1691f56e4fd1128b9d4dc139021fb06181
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-03-31 20:26:23 +0300

    render_image

--- products/admin.py
+++ products/admin.py
@@ -3,36 +3,25 @@ from django.utils.safestring import mark_safe
 from products.models import Product, Category
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('render_image', 'name', 'price', 'is_active')
-    filter_horizontal = ('categories', 'products')
-
-    @staticmethod
-    def render_image(obj):
-        if obj.image:
-            img_html = '<img src="{url}" width="{width}" height="{height}" />'\
-                .format(
-                    url=obj.image.url,
+def render_image(obj):
+    if obj.image:
+        img_html = '<img src="{url}" width="{width}" height="{height}" />' \
+            .format(url=obj.image.url,
                     width=64,
                     height=64
-                )
-            return mark_safe(img_html)
-        return None
+                    )
+        return mark_safe(img_html)
+    return None
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (render_image, 'name', 'price', 'is_active')
+    filter_horizontal = ('categories', 'products')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('render_image', 'name',)
+    list_display = (render_image, 'name',)
 
-    @staticmethod
-    def render_image(obj):
-        if obj.image:
-            img_html = '<img src="{url}" width="{width}" height="{height}" />'\
-                .format(
-                    url=obj.image.url,
-                    width=64,
-                    height=64
-                )
-            return mark_safe(img_html)
-        return None
+
