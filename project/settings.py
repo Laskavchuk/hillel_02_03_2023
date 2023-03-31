a675716a13981967e06ef155afd427ecf81275b8
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-03-31 16:39:39 +0300

    Додав MEDIA_ROOT, MEDIA_URL

--- project/settings.py
+++ project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
+
 import environ
 from pathlib import Path
 
@@ -127,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
