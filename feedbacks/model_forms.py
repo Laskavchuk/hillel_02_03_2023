0c8d51fa389b43a407a7f0d2300eef487a241f5d
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-04 17:18:17 +0300

    FeedbackModelForm, метод clean_text

---
+++ feedbacks/model_forms.py
@@ -0,0 +1,21 @@
+
+from django import forms
+from django.utils.html import strip_tags
+
+from feedbacks.models import Feedback
+
+
+class FeedbackModelForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ('text', 'user', 'rating')
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].widget = forms.HiddenInput()
+        self.fields['user'].initial = user
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        cleaned_text = strip_tags(text)
+        return cleaned_text
