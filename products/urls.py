8252e68baa5bc7d8373036dbbc8cd8fa0a8e94c9
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-08 11:27:12 +0300

    Class ProductView

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import products
+from .views import ProductView
 
 urlpatterns = [
-    path('', products)
+    path('', ProductView.as_view(), name='products'),
 ]
