8ea6a8eb4e8e6fbe86613b4493d6206a1eeff09e
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-11 13:51:02 +0300

    class ProductView

--- products/views.py
+++ products/views.py
@@ -1,5 +1,20 @@
 from django.views.generic import TemplateView
+from .models import Product
+from products.model_forms import ProductModelForm
 
 
 class ProductView(TemplateView):
     template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.iterator()
+        context['form'] = ProductModelForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ProductModelForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+        return self.render_to_response(self.get_context_data())
+
