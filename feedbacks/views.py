5efb021d2fed9c06dcd91829c0075e251c502912
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-11 13:45:52 +0300

    class FeedbackView

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,22 +1,24 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
+from django.views.generic import FormView
 from feedbacks.model_forms import FeedbackModelForm
 from feedbacks.models import Feedback
 
 
-@login_required
-def feedbacks(request, *args, **kwargs):
-    user = request.user
-    if request.method == 'POST':
-        form = FeedbackModelForm(user=user, data=request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = FeedbackModelForm(user=user)
-    context = {
-        'feedbacks': Feedback.objects.all(),
-        'form': form
-    }
-    return render(request, 'feedbacks/index.html', context)
+class FeedbackView(FormView):
+    form_class = FeedbackModelForm
+    template_name = 'feedbacks/index.html'
+    success_url = '/feedbacks/'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['feedbacks'] = Feedback.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
