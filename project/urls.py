ff7402a58cab7984b62b67f730d0650db592e73c
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-11 13:50:35 +0300

    add accounts

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,15 @@ from django.urls import path, include
 from products.urls import urlpatterns as products_urlpatterns
 from django.conf import settings
 from feedbacks.urls import urlpatterns as feedbacks_urlpatterns
+from accounts.urls import urlpatterns as accounts_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include(products_urlpatterns)),
-    path('feedbacks/', include(feedbacks_urlpatterns))
+    path('feedbacks/', include(feedbacks_urlpatterns)),
+    path('accounts/', include(accounts_urlpatterns)),
+
 ]
 if settings.DEBUG:
     from django.conf.urls.static import static
