1311bc9bd15db8096577018b270dbe883d89f360
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-15 15:50:25 +0300

    added widget_tweaks, main

--- project/settings.py
+++ project/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'products',
     'orders',
     'feedbacks',
     'accounts',
+    'main',
 ]
 
 MIDDLEWARE = [
