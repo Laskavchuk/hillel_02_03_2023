ff75822a6e6efa112b287cdbba033e6e2ca19103
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-21 22:25:15 +0300

    added create/

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from feedbacks.views import FeedbackView, FeedbackList
 
 urlpatterns = [
-    path('create/', login_required(FeedbackView.as_view()), name='feedback_create'),
+    path('create/', FeedbackView.as_view(), name='feedback_create'),
     path('', FeedbackList.as_view(), name='feedbacks'),
 ]
