e79cda86d4ae8bfc0e5c8e5ccb169900f0a00a67
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-04-21 19:33:48 +0300

    exp/imp - csv

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductView
-
+from .views import ProductView, ExportCSVView, ImportCSV
+from django.contrib.auth.decorators import login_required, user_passes_test
 urlpatterns = [
     path('', ProductView.as_view(), name='products'),
+    path('export-csv/', ExportCSVView.as_view(), name='products_to_csv'),
+    path('import-csv/', login_required(ImportCSV.as_view()),
+         name='products_from_csv'),
 ]
