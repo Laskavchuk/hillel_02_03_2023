7a35e515ff1d3570d07ccb089954176eab2a1b67
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-06-08 10:08:41 +0300

    rest

--- apis/products/urls.py
+++ apis/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apis.products.views import ProductList, ProductRetrieve1
+from apis.products.views import ProductList, ProductRetrieve
 
 urlpatterns = [
     path('products/', ProductList.as_view()),
